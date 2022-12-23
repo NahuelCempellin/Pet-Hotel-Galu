57bc0cadeb71c88154589a2d60aec41ab0cd2868
nahuel cempellin <nahuelcempellin@gmail.com>
2022-12-23 14:39:05 -0300

    reservas completa sin responsive

--- src/components/PasosDePago/pasos/resumen.jsx
+++ src/components/PasosDePago/pasos/resumen.jsx
@@ -11,14 +11,58 @@ const Resumen = () => {
   }
 
   return (
-    <div>
-      <h1>Resumen</h1>
+    <div className='w-[100%] h-[100%] flex flex-col items-center justify-around mt-5'>
+
+      <div className='flex flex-col items-center  w-[80%] h-[80%] overflow-y-auto'>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Nombre y apellido:</label>
+          <p>Nahuel Cempellin</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Nombre de la mascota: </label>
+          <p>Sora</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Direccion:</label>
+          <p>Tte Gral Peron 2136</p>
+
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Codigo postal:</label>
+          <p>C1006 CABA</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Numero de telefono: </label>
+          <p>223-322054</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Mail: </label>
+          <p>NahuelCempellin@gmail.com</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Fecha de estadia: </label>
+          <p>Desde 23/12/2022 hasta 23/1/2023</p>
+        </div>
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Habitacion:</label>
+          <p>3</p>
+        </div>
+
+        <div className=' w-[100%] p-2 flex items-center justify-between'>
+          <label className='text-orange-500'>Tarjeta n°: </label>
+          <p>XXXX-XXXX-XXXX-1123</p>
+        </div>
+      </div>
+
+      <div  className='w-[90%] flex items-center justify-between'>
       <div>
-        <button onClick={()=>HandlerStep('pagos')}>Atras</button>
+      <button className='p-2 rounded-lg bg-orange-500 w-20 text-white' onClick={()=>HandlerStep('pagos')}>Atras</button>
       </div>
 
       <div>
-        <button>Realizar pago</button>
+        <button className='p-2 rounded-lg bg-orange-500 w-24 text-white' onClick={()=>HandlerStep('')}>Confirmar</button>
+      </div>
+
       </div>
     </div>
   )
